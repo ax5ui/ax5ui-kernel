e01527b08bcd41d8459b5822ec30e4c162075ed8
ThomasJ <tom@axisj.com>
2015-12-01 00:21:00 +0900

    It is building a Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,19 +51,39 @@
 
 	// Project configuration.
 	grunt.initConfig({
+		info: {
+			ax5docs: {
+				css_src: "src/ax5docs/css",
+				css_dest: "src/ax5docs/css"
+			},
+			ax5core: {
+				src: "src/ax5core/js",
+				dest: "src/ax5core/dist",
+				doc_src: "src/ax5docs/_src_ax5core",
+				doc_dest: "src/ax5docs/ax5core"
+			},
+			"bootstrap-ax5dialog": {
+				doc_src: "src/ax5docs/_src_bootstrap-ax5dialog",
+				doc_dest: "src/ax5docs/bootstrap-ax5dialog"
+			}
+		},
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
-			css: {
-				files: ['_src/scss/**/*.scss'],
-				tasks: ['sass:theme_n_demo']
+			ax5docs: {
+				files: ['<%= info["ax5docs"].css_src %>/**/*.scss'],
+				tasks: ['sass:ax5docs']
 			},
-			js: {
-				files: ['_src/**/*.js'],
-				tasks: ['concat']
+			ax5core: {
+				files: ['<%= info["ax5core"].src %>/**/*.js'],
+				tasks: ['concat:ax5docs', 'uglify:ax5core']
 			},
-			tmpl: {
-				files: ['_demo/**/*.html', '_demo/_layouts/*.*'],
-				tasks: ['make-menu-json', 'ax_marko:run_demo']
+			"ax5docs-ax5core": {
+				files: ['<%= info.ax5core.doc_src %>/**/*.html', '<%= info.ax5core.doc_src %>/_layouts/*.*'],
+				tasks: ['make-menu:ax5core', 'ax_marko:ax5core']
+			},
+			"ax5docs-bootstrap-ax5dialog": {
+				files: ['<%= info["bootstrap-ax5dialog"].doc_src %>/**/*.html', '<%= info["bootstrap-ax5dialog"].doc_src %>/_layouts/*.*'],
+				tasks: ['make-menu:bootstrap-ax5dialog', 'ax_marko:bootstrap-ax5dialog']
 			}
 		},
 		concat: {
@@ -73,17 +93,12 @@
 				banner: '/*\n' +
 				' * <%= pkg.name %> - v<%= pkg.version %> \n' +
 				' * <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-				' * www.axisj.com Javascript UI Library\n' +
-				' * \n' +
-				' * Copyright 2013, 2015 AXISJ.com and other contributors \n' +
-				' * Released under the MIT license \n' +
-				' * www.axisj.com/ax5/license \n' +
 				' */\n\n',
 				separator: '\n\n'
 			},
-			dist: {
-				src: ['_src/ax5-polyfill.js', '_src/ax5-core.js', '_src/ax5-ui.js', '_src/ui-classes/*.js'],
-				dest: 'pub/ax5-<%= pkg.version %>.js'
+			ax5core: {
+				src: ['<%= info["ax5core"].src %>/ax5-polyfill.js', '<%= info["ax5core"].src %>/ax5-core.js', '<%= info["ax5core"].src %>/ax5-ui.js'],
+				dest: '<%= info["ax5core"].dest %>/ax5core.js'
 			}
 		},
 		uglify: {
@@ -91,9 +106,9 @@
 				mangle: false,
 				preserveComments: false
 			},
-			publish: {
+			ax5core: {
 				files: {
-					'pub/ax5.min.js': ['pub/ax5-<%= pkg.version %>.js']
+					'<%= info["ax5core"].dest %>/ax5core.min.js': ['<%= info["ax5core"].dest %>/ax5core.js']
 				}
 			}
 		},
@@ -103,11 +118,9 @@
 				outputStyle: 'nested',
 				spawn: false
 			},
-			theme_n_demo: {
+			ax5docs: {
 				files: {
-					'demo-resource/css/demo.css': 'demo-resource/css/demo.scss',
-					/// theme를 확장한다면 json 추가
-					'pub/css/basic/ax5.css': '_src/scss/basic/ax5.scss'
+					'<%= info["ax5docs"].css_dest %>/demo.css': '<%= info["ax5docs"].css_src %>/demo.scss'
 				}
 			}
 		},
@@ -119,15 +132,15 @@
 
 				files: [
 					{
-						src_root: "src/ax5docs/_src_ax5core",
-						src: ["src/ax5docs/_src_ax5core/**/*.html"],
+						src_root: '<%= info.ax5core.doc_src %>',
+						src: ['<%= info.ax5core.doc_src %>/**/*.html'],
 						global_data: { // append data
-							layout_path: "src/ax5docs/_src_ax5core/_layouts/basic.marko", // src relative path
-							output_type: "html"
+							layout_path: '<%= info.ax5core.doc_src %>/_layouts/basic.marko', // src relative path
+							output_type: 'html'
 						},
-						lang: "src/ax5docs/_src_ax5core/_layouts/ko.json",
-						dest: "src/ax5docs/ax5core",
-						output_extension: "html"
+						lang: '<%= info.ax5core.doc_src %>/_layouts/ko.json',
+						dest: '<%= info.ax5core.doc_dest %>',
+						output_extension: 'html'
 					}
 				]
 			},
@@ -138,29 +151,28 @@
 
 				files: [
 					{
-						src_root: "src/ax5docs/_src_bootstrap-ax5dialog",
-						src: ["src/ax5docs/_src_bootstrap-ax5dialog/**/*.html"],
+						src_root: '<%= info["bootstrap-ax5dialog"].doc_src %>',
+						src: ['<%= info["bootstrap-ax5dialog"].doc_src %>/**/*.html'],
 						global_data: { // append data
-							layout_path: "src/ax5docs/_src_bootstrap-ax5dialog/_layouts/basic.marko", // src relative path
-							output_type: "html"
+							layout_path: '<%= info["bootstrap-ax5dialog"].doc_src %>/_layouts/basic.marko', // src relative path
+							output_type: 'html'
 						},
-						lang: "src/ax5docs/_src_bootstrap-ax5dialog/_layouts/ko.json",
-						dest: "src/ax5docs/bootstrap-ax5dialog",
-						output_extension: "html"
+						lang: '<%= info["bootstrap-ax5dialog"].doc_src %>/_layouts/ko.json',
+						dest: '<%= info["bootstrap-ax5dialog"].doc_dest %>',
+						output_extension: 'html'
 					}
 				]
 			}
 		},
-
 		/**
 		 * marko가 만들어낸 html.js 파일을 정리해주는 task
 		 */
 		clean: {
 			marko: [
-				"src/ax5docs/_src_ax5core/**/*.html.js"
+				'<%= info["ax5core"].doc_src %>/**/*.html.js',
+				'<%= info["bootstrap-ax5dialog"].doc_src %>/**/*.html.js'
 			]
 		},
-
 		/**
 		 * demo 파일구조대로 json 생성
 		 */
@@ -168,21 +180,21 @@
 			ax5core: {
 				options: {
 					pwd: '',
-					target: 'src/ax5docs/assets/js/ax5core-menus.js',
+					target: 'src/ax5docs/_assets/js/ax5core-menus.js',
 					url_replace: function(url) {
 						return url.replace("src/ax5docs/_src_", "/");
 					},
-					filter: ['src/ax5docs/_src_ax5core/**/*.html', '!src/ax5docs/_src_ax5core/index.html'] // _demo/index.html 은 제외 하고 모든 html을 대상
+					filter: ['<%= info["ax5core"].doc_src %>/**/*.html', '!<%= info["ax5core"].doc_src %>/index.html'] // _demo/index.html 은 제외 하고 모든 html을 대상
 				}
 			},
 			"bootstrap-ax5dialog": {
 				options: {
 					pwd: '',
-					target: 'src/ax5docs/assets/js/bootstrap-ax5dialog-menus.js',
+					target: 'src/ax5docs/_assets/js/bootstrap-ax5dialog-menus.js',
 					url_replace: function(url) {
 						return url.replace("src/ax5docs/_src_", "/");
 					},
-					filter: ['src/ax5docs/_src_bootstrap-ax5dialog/**/*.html', '!src/ax5docs/_src_bootstrap-ax5dialog/index.html'] // _demo/index.html 은 제외 하고 모든 html을 대상
+					filter: ['<%= info["bootstrap-ax5dialog"].doc_src %>/**/*.html', '!<%= info["bootstrap-ax5dialog"].doc_src %>/index.html'] // _demo/index.html 은 제외 하고 모든 html을 대상
 				}
 			}
 		}
@@ -195,6 +207,8 @@
 	grunt.loadNpmTasks('grunt-ax-marko');
 	grunt.loadNpmTasks('grunt-sass');
 
-	grunt.registerTask('sass-run', ['sass:theme_n_demo', 'watch:css']);
-	grunt.registerTask('tmpl-run', ['make-menu-json', 'ax_marko:ax5core', 'watch:tmpl']);
+	grunt.registerTask('ax5core', ['concat:ax5core', 'uglify:ax5core', 'watch:ax5core']);
+	grunt.registerTask('sass-ax5docs', ['sass:ax5docs', 'watch:ax5docs']);
+	grunt.registerTask('tmpl-ax5core', ['make-menu:ax5core', 'ax_marko:ax5core', 'watch:ax5docs-ax5core']);
+	grunt.registerTask('tmpl-bootstrap-ax5dialog', ['make-menu:bootstrap-ax5dialog', 'ax_marko:bootstrap-ax5dialog', 'watch:ax5docs-bootstrap-ax5dialog']);
 };
