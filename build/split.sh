8dde30e44ef59016f8b5751aa74c17b319d19d8f
ThomasJ <tom@axisj.com>
2015-12-01 16:08:12 +0900

    bash

--- build/split.sh
+++ build/split.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 git subsplit init https://github.com/ax5ui/ax5ui-kernel.git
 git subsplit publish --heads="gh-pages" --no-tags src/ax5docs:https://github.com/ax5ui/ax5docs.git
 git subsplit publish --heads="master" --no-tags src/ax5core:https://github.com/ax5ui/ax5core.git
