10ed77881d0898746e88c0c1d9b786aa2547cec2
ThomasJ <tom@axisj.com>
2015-12-20 22:20:50 +0900

    make gulp remain marko

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var marko = require('marko');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var changed = require('gulp-changed');
+
+var gulp_info = {
+    assets: {
+        src: "src/ax5docs/assets"
+    },
+    ax5docs: {
+        css_src: "src/ax5docs/assets/css",
+        css_dest: "src/ax5docs/assets/css",
+        ax5core: "src/ax5docs/assets/lib/ax5core"
+    },
+    ax5core: {
+        src: "src/ax5core/js",
+        dest: "src/ax5core/dist",
+        doc_src: "src/ax5docs/_src_ax5core",
+        doc_dest: "src/ax5docs/ax5core"
+    },
+    "bootstrap-ax5dialog": {
+        doc_src: "src/ax5docs/_src_bootstrap-ax5dialog",
+        doc_dest: "src/ax5docs/bootstrap-ax5dialog"
+    }
+};
+
+/**
+ * SASS
+ */
+gulp.task('SASS', function () {
+    gulp.src(gulp_info.ax5docs.css_src + '/docs.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest(gulp_info.ax5docs.css_dest));
+    gulp.watch(gulp_info.ax5docs.css_src + '/ * * / *.scss', ['sass']);
+});
+
+/**
+ * for ax5core
+ */
+gulp.task('AX5CORE-scripts', function () {
+    gulp.src(gulp_info.ax5core.src + '/*.js')
+        .pipe(concat('ax5core.js'))
+        .pipe(gulp.dest(gulp_info.ax5core.dest))
+        .pipe(concat('ax5core.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(gulp_info.ax5core.dest));
+
+    gulp.watch(gulp_info.ax5core.src + '/*.js', ['AX5CORE-scripts']);
+});
--- src/ax5core/js/ax5-ui.js
+++ src/ax5core/js/ax5-ui.js
@@ -5,11 +5,11 @@
 
 /**
  * @class ax5.ui.root
- * @classdesc ax5 ui class 코어 클래스 모든 클래스의 공통 함수를 가지고 있습니다.
+ * @classdesc ax5 ui class
  * @version v0.0.1
  * @author tom@axisj.com
  * @logs
- * 2014-12-12 tom : 시작
+ * 2014-12-12 tom : start
  * @example
  * ```
  * var myui = new ax5.ui.root();
@@ -17,60 +17,60 @@
  */
 ax5.ui = (function (core) {
 
-	function axUi() {
-		// 클래스 인스턴스 초기화
-		this.main = (function(){
-			this.config = {};
-			this.name = "root";
-		}).apply(this, arguments);
+    function axUi() {
+        // instance init
+        this.main = (function () {
+            this.config = {};
+            this.name = "root";
+        }).apply(this, arguments);
 
-		/**
-		 * 클래스의 속성 정의 메소드 속성 확장후에 내부에 init 함수를 호출합니다.
-		 * @method ax5.ui.root.setConfig
-		 * @param {Object} config - 클래스 속성값
-		 * @param {Boolean} [callInit=true] - init 함수 호출 여부
-		 * @returns {ax5.ui.axUi}
-		 * @example
-		 * ```
-		 * var myui = new ax5.ui.root();
-		 * myui.setConfig({
+        /**
+         * 클래스의 속성 정의 메소드 속성 확장후에 내부에 init 함수를 호출합니다.
+         * @method ax5.ui.root.setConfig
+         * @param {Object} config - 클래스 속성값
+         * @param {Boolean} [callInit=true] - init 함수 호출 여부
+         * @returns {ax5.ui.axUi}
+         * @example
+         * ```
+         * var myui = new ax5.ui.root();
+         * myui.setConfig({
 		 * 	id:"abcd"
 		 * });
-		 * ```
-		 */
-		this.setConfig = function (cfg, callInit) {
-			core.util.extendAll(this.config, cfg, true);
-			if (typeof callInit == "undefined" || callInit === true) {
-				this.init();
-			}
-			return this;
-		};
-		this.init = function () {
-			console.log(this.config);
-		};
+         * ```
+         */
+        this.setConfig = function (cfg, callInit) {
+            core.util.extendAll(this.config, cfg, true);
+            if (typeof callInit == "undefined" || callInit === true) {
+                this.init();
+            }
+            return this;
+        };
+        this.init = function () {
+            console.log(this.config);
+        };
 
-		this.bindWindowResize = function(callBack){
+        this.bindWindowResize = function (callBack) {
 
-			setTimeout((function(){
-				ax5.dom.resize((function(){
-					if(this.bindWindowResize__) clearTimeout(this.bindWindowResize__);
-					this.bindWindowResize__ = setTimeout((function(){
-						callBack.call(this);
-					}).bind(this), 10);
-				}).bind(this));
-			}).bind(this), 100);
+            setTimeout((function () {
+                ax5.dom.resize((function () {
+                    if (this.bindWindowResize__) clearTimeout(this.bindWindowResize__);
+                    this.bindWindowResize__ = setTimeout((function () {
+                        callBack.call(this);
+                    }).bind(this), 10);
+                }).bind(this));
+            }).bind(this), 100);
 
-		};
+        };
 
-		this.stopEvent = function(e){
-			if (e.preventDefault) e.preventDefault();
-			if (e.stopPropagation) e.stopPropagation();
-			e.cancelBubble = true;
-			return false;
-		}
-	}
+        this.stopEvent = function (e) {
+            if (e.preventDefault) e.preventDefault();
+            if (e.stopPropagation) e.stopPropagation();
+            e.cancelBubble = true;
+            return false;
+        }
+    }
 
-	return {
-		root: axUi
-	}
+    return {
+        root: axUi
+    }
 })(ax5);
