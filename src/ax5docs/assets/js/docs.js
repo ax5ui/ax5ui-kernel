965f00448bb4c75a9d4af9a0e74d4da54df86ae6
ThomasJ <tom@axisj.com>
2015-12-22 12:59:58 +0900

    inline-menu script & style

--- src/ax5docs/assets/js/docs.js
+++ src/ax5docs/assets/js/docs.js
@@ -1,110 +1,109 @@
-// takeover console
-/*
- (function() {
- var console = window.console;
- if (!console) return;
- function intercept(method) {
- var original = console[method];
- console[method] = function() {
+var fn_docs = (function () {
+    return {
+        _data: {},
+        _jos: {}, // jquery objects 저장소
+        menu: {
+            list: [],
+            target: null,
+            scrollType: '',
+            printed: false,
+            ready: function (target, scrollType) {
+                var list = [];
+                this.target = target;
+                this.scrollType = scrollType;
 
- // do sneaky stuff
- if (original.apply) {
- // Do this for normal browsers
- original.apply(console, arguments);
- }
- else {
- // Do this for IE
- var message = Array.prototype.slice.apply(arguments).join(' ');
- original(message);
- }
- }
- }
+                if (scrollType == 'inline') {
+                    var h1_index = -1, h2_index = -1;
+                    fn_docs._jos['docs-body'].find('h1,h2,h3').each(function (idx, nn) {
+                        if(!this.id) this.id = 'docs-' + (new Date()).getTime();
 
- var methods = ['log', 'warn', 'error'];
- for (var i = 0; i < methods.length; i++) intercept(methods[i]);
- })();
- */
+                        if(this.tagName === "H1"){
+                            h1_index++;
+                            _list = list;
+                        }
+                        else if(this.tagName === "H2"){
+                            h2_index++;
+                            _list = (h1_index > -1) ? list[h1_index].child : list;
+                        }
+                        else if(this.tagName == "H3"){
+                            _list = (h1_index > -1 && h2_index > -1) ? list[h1_index].child[h2_index].child : list;
+                        }
 
-var fn_docs = (function() {
-  return {
-    _data: {},
-    _jos: {}, // jquery objects 저장소
-    menu: {
-      print: function(menus) {
-
-        var currentPath = (function() {
-          var pathname = location.pathname;
-          //console.log(pathname, pathname.indexOf('/demo'));
-          pathname = pathname.substr(pathname.indexOf('/demo'));
-          //console.log(pathname);
-          var paths = pathname.split(/\//g);
-          paths.shift();
-          paths.pop();
-          return paths.join('/').split('/').length;
-        })();
-
-        var getUrl = function(url) {
-          var diff_depth = 3 - (url.split(/\//g).length - currentPath);
-          var pre_url = [];
-          for (var i = 0; i < diff_depth; i++) pre_url.push("../");
-          return pre_url.join("") + url;
-        };
-
-        var po = [];
-        for (var i = 0, l = menus.length, menu; i < l; i++) {
-          menu = menus[i];
-          po.push('<li class="docs-menu-group-title">');
-          if (menu.url) {
-            po.push('<a href="' + getUrl(menu.url) + '">' + menu.title + '</a>');
-          }
-          else {
-            po.push(menu.title);
-          }
-          po.push('</li>');
-
-          if (menu.child && menu.child.length > 0) {
-            po.push('<ul class="docs-menu-group-child">');
-
-            menu.child.forEach(function(c) {
-              //console.log(currentPath, c.url);
-              po.push('<li><a href="' + getUrl(c.url) + '">' + c.title + '</a></li>');
-            });
-            po.push('</ul>');
-          }
+                        _list.push({
+                            id: this.id,
+                            label: this.innerHTML,
+                            offset: $(this).offset(),
+                            child: []
+                        });
+                    });
+                }
+                this.list = list;
+                if(!this.printed) this.print();
+            },
+            getMenuList: function (list) {
+                var _this = this,  po = [];
+                list.forEach(function (item) {
+                    po.push('<li>');
+                    po.push('<a href="">' + item.label + "</a>");
+                    po.push('</li>');
+                    if (item.child.length > 0) {
+                        po.push('<ul>');
+                        po.push(_this.getMenuList(item.child));
+                        po.push('</ul>');
+                    }
+                });
+                return po.join('');
+            },
+            print: function () {
+                var po = [];
+                po.push('<ul>');
+                po.push(this.getMenuList(this.list));
+                po.push('</ul>');
+                this.target.html(po.join(''));
+                this.printed = true;
+            }
         }
-        fn_docs._jos["menu-target"].html(po.join(''));
-      }
     }
-  }
 })();
 
-$(document.body).ready(function() {
-  if (!window.fn_docs) return;
+$(document.body).ready(function () {
+    if (!window.fn_docs) return;
 
-  $.ajax({
-    url: "https://api.github.com/repos/ax5ui/ax5core"
-  }).done(function(data) {
-    $("#started-count").html(ax5.util.number(data.stargazers_count, {'money': true})).fadeIn();
-  });
+    $.ajax({
+        url: "https://api.github.com/repos/ax5ui/ax5core"
+    }).done(function (data) {
+        $("#started-count").html(ax5.util.number(data.stargazers_count, {'money': true})).fadeIn();
+    });
 
-  fn_docs._jos = (function() {
-    return {
-      "menu-target": $("#docs-menu-print-target"),
-      "docs-header-tool": $("#docs-header-tool"),
-      "docs-body": $("#docs-body")
-    }
-  })();
-  fn_docs._data["doc-heder-tool-change-position"] = ax5.util.number(fn_docs._jos["docs-body"].css("margin-top")) - fn_docs._jos["docs-header-tool"].height();
+    fn_docs._jos = (function () {
+        return {
+            "menu-target": $("#docs-menu-print-target"),
+            "docs-header-tool": $("#docs-header-tool"),
+            "docs-body": $("#docs-body"),
+            "docs-inline-menu": $("#docs-inline-menu"),
+            "docs-menu": $("#docs-menu")
+        }
+    })();
 
-  // change print way(static) -- remove
-  //fn_docs.menu.print(window.doc_menu_object || []);
+    $(window).on('load resize', function () {
+        // toolbar position cache
+        fn_docs._data["doc-heder-tool-change-position"] = ax5.util.number(fn_docs._jos["docs-body"].css("margin-top")) - fn_docs._jos["docs-header-tool"].height();
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >= fn_docs._data["doc-heder-tool-change-position"]) {
-      fn_docs._jos["docs-header-tool"].addClass("reflection");
-    }
-    else {
-      fn_docs._jos["docs-header-tool"].removeClass("reflection");
-    }
-  });
+        // change print way(static) -- remove
+        if (fn_docs._jos['docs-inline-menu'][0]) {
+            fn_docs.menu.ready(fn_docs._jos['docs-inline-menu'], 'inline');
+        }
+        if (fn_docs._jos['docs-menu'][0]) {
+            fn_docs.menu.ready(fn_docs._jos['docs-menu'], 'static');
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= fn_docs._data["doc-heder-tool-change-position"]) {
+            fn_docs._jos["docs-header-tool"].addClass("reflection");
+        }
+        else {
+            fn_docs._jos["docs-header-tool"].removeClass("reflection");
+        }
+    });
 });
