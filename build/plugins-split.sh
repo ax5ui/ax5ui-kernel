84c0d7a6a39fb772c57dd3bc39c093d5ea2ffe69
ThomasJ <tom@axisj.com>
2016-01-02 01:14:39 +0900

    add bower

---
+++ build/plugins-split.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+git subsplit init https://github.com/ax5ui/ax5ui-kernel.git
+git subsplit publish --heads="master" --no-tags src/ax5core:https://github.com/ax5ui/ax5core.git
+git subsplit publish --heads="master" --no-tags src/bootstrap-ax5mask:https://github.com/ax5ui/bootstrap-ax5mask.git
+git subsplit publish --heads="master" --no-tags src/bootstrap-ax5dialog:https://github.com/ax5ui/bootstrap-ax5dialog.git
+rm -rf .subsplit/
