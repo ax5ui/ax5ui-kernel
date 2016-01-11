16d805e79edab97d97fc45aeb3317b20bbf75e9b
ThomasJ <tom@axisj.com>
2016-01-12 02:38:39 +0900

    toast 99%

--- build/npm-publish.sh
+++ build/npm-publish.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-npm publish src/ax5core &&
-npm publish src/bootstrap-ax5mask &&
+npm publish src/ax5core
+npm publish src/bootstrap-ax5mask
 npm publish src/bootstrap-ax5dialog
+npm publish src/bootstrap-ax5toast
