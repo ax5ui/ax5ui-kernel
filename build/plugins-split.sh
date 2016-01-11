dfa107a42ad62cf7a3c89366d12aaa0c70c8b3e8
ThomasJ <tom@axisj.com>
2016-01-11 16:46:01 +0900

    split

--- build/plugins-split.sh
+++ build/plugins-split.sh
@@ -3,4 +3,5 @@ git subsplit init https://github.com/ax5ui/ax5ui-kernel.git
 git subsplit publish --heads="master" --no-tags src/ax5core:https://github.com/ax5ui/ax5core.git
 git subsplit publish --heads="master" --no-tags src/bootstrap-ax5mask:https://github.com/ax5ui/bootstrap-ax5mask.git
 git subsplit publish --heads="master" --no-tags src/bootstrap-ax5dialog:https://github.com/ax5ui/bootstrap-ax5dialog.git
+git subsplit publish --heads="master" --no-tags src/bootstrap-ax5toast:https://github.com/ax5ui/bootstrap-ax5toast.git
 rm -rf .subsplit/
--- build/split.sh
+++ build/split.sh
@@ -4,4 +4,5 @@ git subsplit publish --heads="gh-pages" --no-tags src/ax5docs:https://github.com
 git subsplit publish --heads="master" --no-tags src/ax5core:https://github.com/ax5ui/ax5core.git
 git subsplit publish --heads="master" --no-tags src/bootstrap-ax5mask:https://github.com/ax5ui/bootstrap-ax5mask.git
 git subsplit publish --heads="master" --no-tags src/bootstrap-ax5dialog:https://github.com/ax5ui/bootstrap-ax5dialog.git
+git subsplit publish --heads="master" --no-tags src/bootstrap-ax5toast:https://github.com/ax5ui/bootstrap-ax5toast.git
 rm -rf .subsplit/
