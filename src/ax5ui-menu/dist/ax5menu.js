27dd9fc2905a546959344967a4bb08707b4ee9da
ThomasJ <tom@axisj.com>
2016-03-13 01:27:05 +0900

    ax5menu

--- src/ax5ui-menu/dist/ax5menu.js
+++ src/ax5ui-menu/dist/ax5menu.js
@@ -1,6 +1,8 @@
+"use strict";
+
 // ax5.ui.menu
 (function (root, _SUPER_) {
-    
+
     /**
      * @class ax5.ui.menu
      * @classdesc
@@ -12,22 +14,21 @@
      * ```
      */
     var U = ax5.util;
-    
+
     //== UI Class
-    var axClass = function () {
-        var
-            self = this,
+    var axClass = function axClass() {
+        var self = this,
             cfg;
-        
+
         if (_SUPER_) _SUPER_.call(this); // 부모호출
 
         this.config = {
             animateTime: 250
         };
-        
+
         this.openTimer = null;
         this.closeTimer = null;
-        
+
         cfg = this.config;
 
         this.init = function () {
@@ -35,38 +36,25 @@
             self.menuId = ax5.getGuid();
         };
 
-
         this.__getTmpl = function () {
-            return `
-            <div class="ax5-ui-menu {{theme}}" id="{{id}}" data-menu-els="root">
-                <div class="ax-menu-body">
-
-                </div>
-                <div class="ax-menu-arrow"></div>
-            </div>
-            `;
+            return "\n            <div class=\"ax5-ui-menu {{theme}}\" id=\"{{id}}\" data-menu-els=\"root\">\n                <div class=\"ax-menu-body\">\n\n                </div>\n                <div class=\"ax-menu-arrow\"></div>\n            </div>\n            ";
         };
 
-        this.popup = function(){
+        this.popup = function () {};
 
-        };
-
-        this.close = function(){
-
-        };
+        this.close = function () {};
 
         // 클래스 생성자
-        this.main = (function () {
+        this.main = function () {
             if (arguments && U.isObject(arguments[0])) {
                 this.setConfig(arguments[0]);
             }
-        }).apply(this, arguments);
+        }.apply(this, arguments);
     };
     //== UI Class
-    
-    root.menu = (function () {
+
+    root.menu = function () {
         if (U.isFunction(_SUPER_)) axClass.prototype = new _SUPER_(); // 상속
         return axClass;
-    })(); // ax5.ui에 연결
-    
+    }(); // ax5.ui에 연결
 })(ax5.ui, ax5.ui.root);
