d8c14ce403e19965c2cad01ccf4e0eb0fec79aca
Brant Hwang <dlstj3039@gmail.com>
2016-04-06 22:46:06 +0900

    modified deploy script added --force option

--- src/ax5ui-calendar/deploy.sh
+++ src/ax5ui-calendar/deploy.sh
@@ -2,6 +2,6 @@ VERSION=`jq '.version' package.json | sed -e 's/^"//'  -e 's/"$//'`
 
 git tag -a $VERSION -m $VERSION
 
-git push origin master --follow-tags
+git push origin HEAD:master --follow-tags --force
 
 npm publish
