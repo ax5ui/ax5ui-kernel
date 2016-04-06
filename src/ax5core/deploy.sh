72b3e59590e684ccbe5706f9da99e1790c8b1153
Brant Hwang <dlstj3039@gmail.com>
2016-04-06 12:26:02 +0900

    add deploy script~

--- src/ax5core/deploy.sh
+++ src/ax5core/deploy.sh
@@ -1,7 +1,7 @@
 VERSION=`jq '.version' package.json | sed -e 's/^"//'  -e 's/"$//'`
 
-git tag -a VERSION -m VERSION
+git tag -a $VERSION -m $VERSION
 
-git push origin master
+git push origin master --follow-tags
 
 npm publish
