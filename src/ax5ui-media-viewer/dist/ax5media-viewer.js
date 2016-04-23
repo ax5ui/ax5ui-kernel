b261a19b0ec7abe170a1b7c854a48790f9e58e5d
Thomas Jang <tom@axisj.com>
2016-04-24 00:17:23 +0900

    ax5ui-media-viewer

---
+++ src/ax5ui-media-viewer/dist/ax5media-viewer.js
@@ -0,0 +1,107 @@
+'use strict';
+
+/*
+ * Copyright (c) 2016. tom@axisj.com
+ * - github.com/thomasjang
+ * - www.axisj.com
+ */
+
+// ax5.ui.media-viewer
+(function (root, _SUPER_) {
+
+    /**
+     * @class ax5.ui.mediaViewer
+     * @classdesc
+     * @version 0.0.1
+     * @author tom@axisj.com
+     * @example
+     * ```
+     * var mymediaViewer = new ax5.ui.mediaViewer();
+     * ```
+     */
+    var U = ax5.util;
+
+    //== UI Class
+    var axClass = function axClass() {
+        var self = this,
+            cfg;
+
+        if (_SUPER_) _SUPER_.call(this); // 부모호출
+
+        this.queue = [];
+        this.config = {
+            clickEventName: "click", //(('ontouchstart' in document.documentElement) ? "touchend" : "click"),
+            theme: 'default',
+            title: '',
+            animateTime: 250,
+
+            lang: {},
+            columnKeys: {}
+        };
+
+        this.openTimer = null;
+        this.closeTimer = null;
+
+        cfg = this.config;
+
+        var onStateChanged = function onStateChanged(opts, that) {
+            if (opts && opts.onStateChanged) {
+                opts.onStateChanged.call(that, that);
+            } else if (this.onStateChanged) {
+                this.onStateChanged.call(that, that);
+            }
+            return true;
+        },
+            getFrameTmpl = function getFrameTmpl() {
+            return '\n\n                ';
+        },
+            getThumbnailTmpl = function getThumbnailTmpl() {
+            return '\n\n                ';
+        },
+            onClick = function onClick(e, target) {
+            target = U.findParentNode(e.target, function (target) {
+                if (target.getAttribute("data-picker-els")) {
+                    return true;
+                } else if (opts.$target.get(0) == target) {
+                    return true;
+                }
+            });
+            if (!target) {
+                return this;
+            }
+            return this;
+        };
+        /// private end
+
+        /**
+         * Preferences of mediaViewer UI
+         * @method ax5.ui.mediaViewer.setConfig
+         * @param {Object} config - 클래스 속성값
+         * @returns {ax5.ui.mediaViewer}
+         * @example
+         * ```
+         * ```
+         */
+        this.init = function () {
+            this.onStateChanged = cfg.onStateChanged;
+            this.onClick = cfg.onClick;
+
+            jQuery(window).bind("resize.ax5media-viewer", function () {}.bind(this));
+        };
+
+        // 클래스 생성자
+        this.main = function () {
+            if (arguments && U.isObject(arguments[0])) {
+                this.setConfig(arguments[0]);
+            } else {
+                this.init();
+            }
+        }.apply(this, arguments);
+    };
+    //== UI Class
+
+    root.mediaViewer = function () {
+        if (U.isFunction(_SUPER_)) axClass.prototype = new _SUPER_(); // 상속
+        return axClass;
+    }(); // ax5.ui에 연결
+})(ax5.ui, ax5.ui.root);
