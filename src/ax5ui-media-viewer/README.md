cf32e13e3eae34303108b314a90b799b9996a75d
Thomas Jang <tom@axisj.com>
2016-04-23 23:31:12 +0900

    ax5ui-media-viewer

--- src/ax5ui-media-viewer/README.md
+++ src/ax5ui-media-viewer/README.md
@@ -1,17 +1,16 @@
 [![axisj-contributed](https://img.shields.io/badge/AXISJ.com-Contributed-green.svg)](https://github.com/axisj)
 ![](https://img.shields.io/badge/Seowoo-Mondo&Thomas-red.svg)
 
-# ax5ui-select
+# ax5ui-media-viewer
 
 > *Dependencies*
 > * _[jQuery 1.X+](http://jquery.com/)_
 > * _[ax5core](http://ax5.io/ax5core)_
 > * _[bootstrap](http://getbootstrap.com/)_
 
-
 ### Install by bower
 ```sh
-bower install ax5ui-select
+bower install ax5ui-media-viewer
 ```
 [bower](http://bower.io/#install-bower) is web front-end package manager.
 using the `bower`, when you install the plug-in is installed to resolve the plug-in dependencies under the `bower_components` folder.  
@@ -22,21 +21,21 @@ If you've never used a bower is, you will be able to be used for [http://bower.i
 
 ### Download code
 - [ax5core Github releases](https://github.com/ax5ui/ax5core/releases)
-- [ax5ui-select Github releases](https://github.com/ax5ui/ax5ui-select/releases)
+- [ax5ui-media-viewer Github releases](https://github.com/ax5ui/ax5ui-media-viewer/releases)
 
 
-### Insert the "ax5select" in the HTML HEAD.
+### Insert the "ax5media-viewer" in the HTML HEAD.
 
 Location of the folder can be determined freely in your project. But be careful not to accidentally caused
 exactly the path.
 ```html
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="bower_components/ax5ui-select/dist/ax5select.css" />
+        <link rel="stylesheet" type="text/css" href="bower_components/ax5ui-media-viewer/dist/ax5media-viewer.css" />
     
         <script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
         <script type="text/javascript" src="bower_components/ax5core/dist/ax5core.min.js"></script>
-        <script type="text/javascript" src="bower_components/ax5ui-select/dist/ax5select.min.js"></script>
+        <script type="text/javascript" src="bower_components/ax5ui-media-viewer/dist/ax5media-viewer.min.js"></script>
     </head>
 <body>
 
@@ -46,9 +45,9 @@ exactly the path.
 
 ### USE CDN
 ```
-https://cdn.rawgit.com/ax5ui/ax5ui-select/master/dist/ax5select.css
-https://cdn.rawgit.com/ax5ui/ax5ui-select/master/dist/ax5select.js
-https://cdn.rawgit.com/ax5ui/ax5ui-select/master/dist/ax5select.min.js
+https://cdn.rawgit.com/ax5ui/ax5ui-media-viewer/master/dist/ax5media-viewer.css
+https://cdn.rawgit.com/ax5ui/ax5ui-media-viewer/master/dist/ax5media-viewer.js
+https://cdn.rawgit.com/ax5ui/ax5ui-media-viewer/master/dist/ax5media-viewer.min.js
 ```
 
 ### Basic Usages
@@ -65,7 +64,7 @@ In npm, so pile on the package manager for the front end, you need to solve the 
 ```sh
 npm install jquery
 npm install ax5core
-npm install ax5ui-select
+npm install ax5ui-media-viewer
 ```
 
 After you download the file in npm install, you will need to copy to the location where you want to use as a resource for the project.
@@ -73,6 +72,6 @@ If the inconvenience in the process that you want to copy the file and can be ea
 ***
 
 ### Preview
-- [See Demostration](http://ax5.io/ax5ui-select/demo/index.html)
+- [See Demostration](http://ax5.io/ax5ui-media-viewer/demo/index.html)
 
 If you have any questions, please refer to the following [gitHub](https://github.com/ax5ui/ax5ui-kernel)
---
+++ src/ax5ui-media-viewer/src/ax5media-viewer.js
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2016. tom@axisj.com
+ * - github.com/thomasjang
+ * - www.axisj.com
+ */
+
+// ax5.ui.media-viewer
+(function (root, _SUPER_) {
+
+    /**
+     * @class ax5.ui.mediaViewer
+     * @classdesc
+     * @version 0.0.1
+     * @author tom@axisj.com
+     * @example
+     * ```
+     * var mymediaViewer = new ax5.ui.mediaViewer();
+     * ```
+     */
+    var U = ax5.util;
+
+    //== UI Class
+    var axClass = function () {
+        var
+            self = this,
+            cfg;
+
+        if (_SUPER_) _SUPER_.call(this); // 부모호출
+
+        this.queue = [];
+        this.config = {
+            clickEventName: "click", //(('ontouchstart' in document.documentElement) ? "touchend" : "click"),
+            theme: 'default',
+            title: '',
+            animateTime: 250,
+
+            lang: {
+                
+            },
+            columnKeys: {
+                
+            }
+        };
+
+        this.openTimer = null;
+        this.closeTimer = null;
+
+        cfg = this.config;
+
+        var
+            onStateChanged = function (opts, that) {
+                if (opts && opts.onStateChanged) {
+                    opts.onStateChanged.call(that, that);
+                }
+                else if (this.onStateChanged) {
+                    this.onStateChanged.call(that, that);
+                }
+                return true;
+            },
+            getTmpl = function () {
+                return `
+
+                `;
+            },
+            onBodyClick = function (e, target) {
+                target = U.findParentNode(e.target, function (target) {
+                    if (target.getAttribute("data-picker-els")) {
+                        return true;
+                    }
+                    else if (opts.$target.get(0) == target) {
+                        return true;
+                    }
+                });
+                if (!target) {
+                    
+                    return this;
+                }
+                return this;
+            },
+            onBodyKeyup = function (e) {
+                if (e.keyCode == ax5.info.eventKeys.ESC) {
+                    this.close();
+                }
+            };
+        /// private end
+
+        /**
+         * Preferences of mediaViewer UI
+         * @method ax5.ui.mediaViewer.setConfig
+         * @param {Object} config - 클래스 속성값
+         * @returns {ax5.ui.mediaViewer}
+         * @example
+         * ```
+         * ```
+         */
+        this.init = function () {
+            this.onStateChanged = cfg.onStateChanged;
+            jQuery(window).bind("resize.ax5media-viewer", (function () {
+                
+            }).bind(this));
+        };
+        
+        // 클래스 생성자
+        this.main = (function () {
+            if (arguments && U.isObject(arguments[0])) {
+                this.setConfig(arguments[0]);
+            }
+            else {
+                this.init();
+            }
+        }).apply(this, arguments);
+    };
+    //== UI Class
+
+    root.mediaViewer = (function () {
+        if (U.isFunction(_SUPER_)) axClass.prototype = new _SUPER_(); // 상속
+        return axClass;
+    })(); // ax5.ui에 연결
+
+})(ax5.ui, ax5.ui.root);
