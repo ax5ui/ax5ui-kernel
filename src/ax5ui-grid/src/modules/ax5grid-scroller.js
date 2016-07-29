69f5ddb11bc7ff6ab24244b16849b715b3eda656
Thomas Jang <tom@axisj.com>
2016-07-29 12:47:59 +0900

    ax5grid : scroller scrollBar moving

--- src/ax5ui-grid/src/modules/ax5grid-scroller.js
+++ src/ax5ui-grid/src/modules/ax5grid-scroller.js
@@ -6,18 +6,48 @@
 
     var scrollMover = {
         "on": function (track, bar, type) {
-            var self = this;
-            // console.log(this.xvar.mousePosition);
+            var self = this,
+                barOffset = bar.position(),
+                barBox = {
+                    width: bar.width(), height: bar.height()
+                },
+                trackBox = {
+                    width: track.innerWidth(), height: track.innerHeight()
+                },
+                getScrollerPosition = {
+                    "vertical": function (e) {
+                        var mouseObj = root.util.getMousePosition(e);
+                        self.xvar.__da = mouseObj.clientY - self.xvar.mousePosition.clientY;
+                        // track을 벗어 나지 안도록 범위 체크
+                        var newTop = barOffset.top + self.xvar.__da;
+                        if (newTop < 0) {
+                            newTop = 0;
+                        }
+                        else if ((newTop + barBox.height) > trackBox.height) {
+                            newTop = trackBox.height - barBox.height;
+                        }
+                        return {top: newTop};
+                    },
+                    "horizontal": function (e) {
+                        var mouseObj = root.util.getMousePosition(e);
+                        self.xvar.__da = mouseObj.clientX - self.xvar.mousePosition.clientX;
+                        // track을 벗어 나지 안도록 범위 체크
+                        var newLeft = barOffset.left + self.xvar.__da;
+                        if (newLeft < 0) {
+                            newLeft = 0;
+                        }
+                        else if ((newLeft + barBox.width) > trackBox.width) {
+                            newLeft = trackBox.width - barBox.width;
+                        }
+                        return {left: newLeft};
+                    }
+                };
 
-            self.xvar.__da = 0;
-                jQuery(document.body)
+            self.xvar.__da = 0; // 이동량 변수 초기화 (계산이 잘못 될까바)
+
+            jQuery(document.body)
                 .bind(root.util.ENM["mousemove"] + ".ax5grid-" + this.instanceId, function (e) {
-                    //self.resizer.css(getResizerPosition[panel.resizerType](e));
-                    var mouseObj = root.util.getMousePosition(e);
-                    self.xvar.__da = mouseObj.clientY - self.xvar.mousePosition.clientY;
-                    
-                    console.log(self.xvar.__da);
-                    
+                    bar.css(getScrollerPosition[type](e));
                 })
                 .bind(root.util.ENM["mouseup"] + ".ax5grid-" + this.instanceId, function (e) {
                     scrollMover.off.call(self);
@@ -54,7 +84,7 @@
         this.$["scroller"]["vertical-bar"].css({width: this.config.scroller.size - (margin + 1), left: margin / 2});
         this.$["scroller"]["horizontal-bar"].css({height: this.config.scroller.size - (margin + 1), top: margin / 2});
 
-        this.$["scroller"]["vertical"]
+        this.$["scroller"]["vertical-bar"]
             .bind(root.util.ENM["mousedown"], (function (e) {
                 this.xvar.mousePosition = root.util.getMousePosition(e);
                 scrollMover.on.call(this, this.$["scroller"]["vertical"], this.$["scroller"]["vertical-bar"], "vertical");
@@ -63,7 +93,25 @@
                 U.stopEvent(e);
                 return false;
             });
-        // todo : scroller scroll ready
+        this.$["scroller"]["vertical"]
+            .bind("click", (function (e) {
+
+            }).bind(this));
+
+        this.$["scroller"]["horizontal-bar"]
+            .bind(root.util.ENM["mousedown"], (function (e) {
+                this.xvar.mousePosition = root.util.getMousePosition(e);
+                scrollMover.on.call(this, this.$["scroller"]["horizontal"], this.$["scroller"]["horizontal-bar"], "horizontal");
+            }).bind(this))
+            .bind("dragstart", function (e) {
+                U.stopEvent(e);
+                return false;
+            });
+        this.$["scroller"]["horizontal"]
+            .bind("click", (function (e) {
+
+            }).bind(this));
+
     };
 
     var resize = function () {
