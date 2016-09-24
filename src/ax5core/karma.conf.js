2fed3f2b5aac86a3cd55dc64b8bb5f015799860b
HJ.Park <hyunjun19@gmail.com>
2016-09-24 17:48:33 +0900

    refactoring test structure

--- src/ax5core/karma.conf.js
+++ src/ax5core/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             'dist/ax5core.js',
-            'karma.test.js'
+            'test.core.js'
         ],
         // list of files to exclude
         exclude: [],
