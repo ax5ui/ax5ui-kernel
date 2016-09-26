dde1ee0c9ad25de17dc76baa22cce587d425bc13
hoksi2k <hoksi2k@hanmail.net>
2016-09-26 19:46:51 +0900

    1. PHP AX5 그리드 연동 샘플 프로그램 작성 2. AX5 그리드 AJAX 연동 샘플 작성

--- src/ax5ui-grid/test/php/index.php
+++ src/ax5ui-grid/test/php/index.php
@@ -1,23 +1,11 @@
-<?php require 'vendor/autoload.php';
+<?php
+/**
+ * PHP를 이용한 AX5 그리드 샘플 프로그램
+ */
+
 
 // 기본 설정
-$bower_url = '../../bower_components';
-$ax5_url = '../../../dist';
+$bower_url = '../bower_components';
+$ax5_url = '../../dist';
 
-$router = new AltoRouter();
-$router->setBasePath('/ax5ui-grid/test/php/index.php');
-
-// 라우팅 설정
-$router->map('GET', '/', function() use ($bower_url, $ax5_url) {
-    require __DIR__ . '/views/grid.php';
-});
-
-// 매핑 함수 실행
-$match = $router->match();
-
-if( $match && is_callable( $match['target'] ) ) {
-    call_user_func_array( $match['target'], $match['params'] );
-} else {
-    // no route was matched
-    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-}
+require __DIR__ . '/views/grid.php';
