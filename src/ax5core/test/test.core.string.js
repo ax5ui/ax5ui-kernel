292c3c042b371f39a8a3d9c7a98fe4338b2ab950
금대건 <daegun@mathtriz.com>
2016-09-26 23:02:22 +0900

    modify ax5.util.left

--- src/ax5core/test/test.core.string.js
+++ src/ax5core/test/test.core.string.js
@@ -1,17 +1,114 @@
 describe('ax5.util.left TEST', function() {
   var testCases = [
       {
-          args: [ 'abcd.efd', 3 ],
-          expect: 'abc'
+          args: [ 'ab.da', '.' ],
+          expect: 'ab',
+          explanation: 'ab.da, "."'
       },
       {
-          args: [ 'abcd.efd', '.' ],
-          expect: 'abcd'
+          args: [ 'abc"', "\""],
+          expect: 'abc',
+          explanation: 'abc", "\""'
+      },
+      {
+          args: [ 'abc4', 4 ],
+          expect: 'abc4',
+          explanation: 'abc4, 4'
+      },
+      {
+          args: [ 'abc4', 4.0 ],
+          expect: 'abc4',
+          explanation: 'abc4, 4.0'
+      },
+      {
+          args: [ 'abc4', 4.1 ],
+          expect: 'abc4',
+          explanation: 'abc4, 4.1'
+      },
+      {
+          args: [ 'abc4', 4.9 ],
+          expect: 'abc4',
+          explanation: 'abc4, 4.9'
+      },
+      {
+          args: [ 'abc4', 100 ],
+          expect: 'abc4',
+          explanation: 'abc4, 100'
+      },
+      {
+          args: [ 'abc4', '4' ],
+          expect: 'abc',
+          explanation: 'abc4, "4"'
+      },
+      {
+          args: [ 'abc', '4' ],
+          expect: '',
+          explanation: 'abc, "4"'
+      },
+      {
+          args: [ 'abc', 0],
+          expect: '',
+          explanation: 'abc", 0'
+      },
+      {
+          args: [ 'abc', '0'],
+          expect: '',
+          explanation: 'abc", "0"'
+      },
+      {
+          args: [ 'abc'],
+          expect: '',
+          explanation: 'abc'
+      },
+      {
+          args: [ 'abc', '' ],
+          expect: '',
+          explanation: 'abc, ""'
+      },
+      {
+          args: [ 'abc', null ],
+          expect: '',
+          explanation: 'abc, ""'
+      },
+      {
+          args: [ 'abc', undefined],
+          expect: '',
+          explanation: 'abc", undefined'
+      },
+      {
+          args: [ 'abc', true],
+          expect: '',
+          explanation: 'abc", true'
+      },
+      {
+          args: [ 'abc', false],
+          expect: '',
+          explanation: 'abc", false'
+      },
+      {
+          args: [ 'abc', new Object],
+          expect: '',
+          explanation: 'abc", new Object'
+      },
+      {
+          args: [ 'abc', new Function],
+          expect: '',
+          explanation: 'abc", new Function]'
+      },
+      {
+          args: [ 'abc', []],
+          expect: '',
+          explanation: 'abc", []]'
+      },
+      {
+          args: [ 'abc', {}],
+          expect: '',
+          explanation: 'abc", {}]'
       }
   ];
 
   testCases.forEach(function(testCase){
-      it('ax5.util.left(' + testCase.args + ') expect ' + testCase.expect, function() {
+      it('ax5.util.left(' + testCase.explanation + ') expect ' + testCase.expect, function() {
           var actual = ax5.util.left.apply(this, testCase.args);
 
           actual.should.deepEqual(testCase.expect);
