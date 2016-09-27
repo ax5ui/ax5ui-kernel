633c95a2244d406f64d007305a701d17731ae51f
HJ.Park <hyunjun19@gmail.com>
2016-09-27 13:22:49 +0900

    add ax5.info TEST

---
+++ src/ax5core/test/test.core.info.js
@@ -0,0 +1,67 @@
+
+// ax5.info.ETC
+// ax5.info.supportTouch
+describe('ax5.info TEST', function() {
+    it('ax5.info.version expect x.x.x', function() {
+        should.equal(ax5.info.version.split('.').length, 3);
+    });
+
+    it('ax5.info.eventKeys expect {BACKSPACE: 8, ...}', function() {
+        var expect = {
+            "BACKSPACE": 8,
+            "TAB": 9,
+            "RETURN": 13,
+            "ESC": 27,
+            "LEFT": 37,
+            "UP": 38,
+            "RIGHT": 39,
+            "DOWN": 40,
+            "DELETE": 46,
+            "HOME": 36,
+            "END": 35,
+            "PAGEUP": 33,
+            "PAGEDOWN": 34,
+            "INSERT": 45,
+            "SPACE": 32
+        };
+        should.deepEqual(ax5.info.eventKeys, expect);
+    });
+
+    it('ax5.info.browser expect {name: "xxx", version: "xx", mobile: Boolean}', function() {
+        should.deepEqual(Object.keys(ax5.info.browser), ["name", "version", "mobile"]);
+    });
+
+    it('ax5.info.urlUtil() expect ["href", "param", ...]', function() {
+        should.deepEqual(Object.keys(ax5.info.urlUtil()), ["href", "param", "referrer", "pathname", "hostname", "port", "url", "baseUrl"]);
+    });
+
+    it('ax5.info.errorMsg.ax5dialog["501"] expect "Duplicate call error"', function() {
+        should.equal(ax5.info.errorMsg.ax5dialog["501"], 'Duplicate call error');
+    });
+
+    it('ax5.info.isBrowser expect true', function() {
+        should.equal(ax5.info.isBrowser, true);
+    });
+
+    it('ax5.info.wheelEnm expect "mousewheel"', function() {
+        should.equal(ax5.info.wheelEnm, "mousewheel");
+    });
+
+    it('ax5.info.weekNames expect [{label: "SUN"},{label: "MON"}...]', function() {
+        var expect = [
+            { "label": "SUN" },
+            { "label": "MON" },
+            { "label": "TUE" },
+            { "label": "WED" },
+            { "label": "THU" },
+            { "label": "FRI" },
+            { "label": "SAT" }
+        ];
+        should.deepEqual(ax5.info.weekNames, expect);
+    });
+
+    it('ax5.info.supportTouch expect false', function() {
+        should.equal(ax5.info.supportTouch, false);
+    });
+
+});
