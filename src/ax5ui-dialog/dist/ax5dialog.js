226a775d375e7d9c2d6336f7b2603429e771e0ce
groovedk <root@axisj.com>
2016-09-27 15:40:31 +0900

    refactor: extract ax5dialog template code edit

--- src/ax5ui-dialog/dist/ax5dialog.js
+++ src/ax5ui-dialog/dist/ax5dialog.js
@@ -50,50 +50,7 @@
                 that = null;
                 return true;
             },
-
-
-            /*
-            getContentTmpl = function () {
-                return `
-                <div id="{{dialogId}}" data-ax5-ui="dialog" class="ax5-ui-dialog {{theme}}">
-                    <div class="ax-dialog-header">
-                        {{{title}}}
-                    </div>
-                    <div class="ax-dialog-body">
-                        <div class="ax-dialog-msg">{{{msg}}}</div>
-                        
-                        {{#input}}
-                        <div class="ax-dialog-prompt">
-                            {{#@each}}
-                            <div class="form-group">
-                            {{#@value.label}}
-                            <label>{{#_crlf}}{{{.}}}{{/_crlf}}</label>
-                            {{/@value.label}}
-                            <input type="{{@value.type}}" placeholder="{{@value.placeholder}}" class="form-control {{@value.theme}}" data-dialog-prompt="{{@key}}" style="width:100%;" value="{{@value.value}}" />
-                            {{#@value.help}}
-                            <p class="help-block">{{#_crlf}}{{.}}{{/_crlf}}</p>
-                            {{/@value.help}}
-                            </div>
-                            {{/@each}}
-                        </div>
-                        {{/input}}
-                        
-                        <div class="ax-dialog-buttons">
-                            <div class="ax-button-wrap">
-                            {{#btns}}
-                                {{#@each}}
-                                <button type="button" data-dialog-btn="{{@key}}" class="btn btn-{{@value.theme}}">{{@value.label}}</button>
-                                {{/@each}}
-                            {{/btns}}
-                            </div>
-                        </div>
-                    </div>
-                </div>  
-                `;
-            },
-            */
-
-            getContent = function getContent(dialogId, opts) {
+                getContent = function getContent(dialogId, opts) {
                 var data = {
                     dialogId: dialogId,
                     title: opts.title || cfg.title || "",
@@ -106,7 +63,6 @@
                 };
 
                 try {
-                    //return ax5.mustache.render(getContentTmpl(), data);
                     return DIALOG.tmpl.get.call(this, "dialogDisplay", data);
                 } finally {
                     data = null;
