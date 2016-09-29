0d6e03df6deb68a00fc063c0a2c4d492acb1e97d
Lee Han Bin <leehan0617@naver.com>
2016-09-29 10:41:53 +0900

    add ax5.ui.mask.open , ax5.ui.mask.close test (#57)
    * add ax5.ui.mask.open , ax5.ui.mask.close test
    
    * add ax5.ui.mask theme testing

--- src/ax5ui-mask/test/test.mask.js
+++ src/ax5ui-mask/test/test.mask.js
@@ -2,9 +2,8 @@
 	test.mask.js
 	TODO event test 
 */
-
 /* ax5.mask.setConfig */
-describe('ax5.mask.setConfig TEST', function(){
+describe('ax5.ui.mask.setConfig TEST', function(){
 	it('using setConfig', function() {
     	var mask = new ax5.ui.mask();
 			mask.setConfig({
@@ -38,7 +37,43 @@ describe('ax5.mask.setConfig TEST', function(){
 		should.deepEqual(mask.config.content , 'Loading content');
     });
 });
+/* end ax5.mask.setConfig */
 
 /* ax5.mask.open */
-describe('ax5.mask.open TEST' , function(){
+describe('ax5.ui.mask.open TEST' , function(){
+	it('ax5.ui.mask.open config' , function(){
+		var mask = new ax5.ui.mask();
+		should.deepEqual(typeof mask.open , "function");
+	});
+
+	it('ax5.ui.mask.open with config' , function(){
+		var mask = new ax5.ui.mask({
+			target : $('body').get(0),
+			content : 'mask'
+		});
+		should.deepEqual(typeof mask.open , "function");
+	});
 });
+/* end ax5.mask.open */
+
+/* ax5.mask.close*/
+describe('ax5.ui.mask.close TEST' , function(){
+	it('ax5.ui.mask.close config' , function(){
+		var mask = new ax5.ui.mask();
+
+		should.deepEqual(typeof mask.close , 'function');
+	});
+});
+/* end ax5.mask.close*/
+
+/* ax5.mask theme test */
+describe('ax5.ui.mask theme TEST' , function(){
+	it('ax5.ui.mask theme TEST' , function(){
+		var mask = new ax5.ui.mask({
+			theme : 'danger'
+		});
+
+		should.deepEqual(mask.config.theme , 'danger');
+	});
+});
+/* end ax5.mask theme test */
