d27cc2c27e04ae7c5142d969c054d5a34eb79cef
hoksi2k <hoksi2k@hanmail.net>
2016-09-30 14:15:22 +0900

    그리드 테스트용 PHP 클래스 추가

---
+++ src/ax5ui-grid/test/php/lib/Grid.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * User: user
+ * Date: 2016-09-30
+ * Time: 오전 10:02
+ */
+class Grid
+{
+
+}
+
+class Header
+{
+
+}
+
+class Column
+{
+    public $key;
+    public $label;
+    public $formatter;
+    public $align;
+    public $width;
+    public $enableFilter;
+}
