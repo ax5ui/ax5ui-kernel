b81a7a93d482c43cc070d430cd5cba24ca3d62cd
Brant Hwang <dlstj3039@gmail.com>
2016-10-17 19:38:54 +0900

    update deploy script

--- src/ax5core/deploy.sh
+++ src/ax5core/deploy.sh
@@ -1,14 +1,18 @@
-LOG=git log -p -1
+LOG=`git log --pretty=oneline --abbrev-commit -1`
 
 case "$LOG" in
-  *MAJOR@*) VERSION=$(npm version major) ;;
-  *MINOR@*) VERSION=$(npm version minor) ;;
-  *)       VERSION=$(npm version patch) ;;
+  *MAJOR@*) echo "Major Version!"; VERSION=$(npm version major) ;;
+  *MINOR@*) echo "Minor Version!";VERSION=$(npm version minor) ;;
+  *)       echo "Patch Version!";VERSION=$(npm version patch) ;;
 esac
 
 VERSION=$(echo $VERSION | cut -c 2-)
 
-gulp build
+npm install && gulp
+
+git add *
+
+git commit -m "$VERSION Released" && git push origin master
 
 git tag -a $VERSION -m $VERSION || true
 
