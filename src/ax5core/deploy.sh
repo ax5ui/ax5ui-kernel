357b2ead195807d612fd2703b95b567996dab782
Brant Hwang <dlstj3039@gmail.com>
2016-10-17 19:34:39 +0900

    update Major Minor Match string

--- src/ax5core/deploy.sh
+++ src/ax5core/deploy.sh
@@ -1,4 +1,14 @@
-VERSION=`jq '.version' package.json | sed -e 's/^"//'  -e 's/"$//'`
+LOG=git log -p -1
+
+case "$LOG" in
+  *[MAJOR]*) VERSION=$(npm version major) ;;
+  *[MINOR]*) VERSION=$(npm version minor) ;;
+  *)       VERSION=$(npm version patch) ;;
+esac
+
+VERSION=$(echo $VERSION | cut -c 2-)
+
+gulp build
 
 git tag -a $VERSION -m $VERSION || true
 
