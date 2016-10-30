983eae473206c8ff167fcfc3f095b81e252e17d1
Thomas Jang <tom@axisj.com>
2016-10-30 23:40:43 +0900

    1.3.10 RELEASED

--- src/ax5ui-combobox/dist/ax5combobox.js
+++ src/ax5ui-combobox/dist/ax5combobox.js
@@ -9,7 +9,7 @@
 
     UI.addClass({
         className: "combobox",
-        version: "1.3.9"
+        version: "1.3.10"
     }, function () {
         /**
          * @class ax5combobox
--- src/ax5ui-multi-uploader/dist/ax5multi-uploader.js
+++ src/ax5ui-multi-uploader/dist/ax5multi-uploader.js
@@ -8,7 +8,7 @@
 
     UI.addClass({
         className: "multiUploader",
-        version: "1.3.9"
+        version: "1.3.10"
     }, function () {
         /**
          * @class ax5multiUploader
