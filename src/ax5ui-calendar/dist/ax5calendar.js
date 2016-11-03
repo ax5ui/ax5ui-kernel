0a883771b0d91d5001ae1629a04c1c3a5a38b5ac
Thomas Jang <tom@axisj.com>
2016-11-03 17:18:47 +0900

    1.3.20 RELEASED

--- src/ax5ui-calendar/dist/ax5calendar.js
+++ src/ax5ui-calendar/dist/ax5calendar.js
@@ -9,7 +9,7 @@
 
     UI.addClass({
         className: "calendar",
-        version: "1.3.19"
+        version: "1.3.20"
     }, function () {
 
         /**
--- src/ax5ui-multi-uploader/dist/ax5multi-uploader.js
+++ src/ax5ui-multi-uploader/dist/ax5multi-uploader.js
@@ -8,7 +8,7 @@
 
     UI.addClass({
         className: "multiUploader",
-        version: "1.3.19"
+        version: "1.3.20"
     }, function () {
         /**
          * @class ax5multiUploader
