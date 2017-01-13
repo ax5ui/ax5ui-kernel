24414d9faa676df56a29639c226688dcc99f54fc
aeei <khm@imtsoft.org>
2017-01-13 18:10:20 +0900

    update test code

--- src/ax5ui-formatter/test/test.formatter.js
+++ src/ax5ui-formatter/test/test.formatter.js
@@ -70,3 +70,61 @@ describe('formatter Date TEST', function() {
 		$('div.form-group').remove();
 	});
 });
+
+describe('formatter Phone TEST', function() {
+	before(function(){
+		$('body').append(
+			'<div class="form-group" id="ax5formatter-01">\n' +
+			'    <div class="input-group">\n' +
+			'        <span class="input-group-addon">Money</span>\n' +
+			'        <input id="ax5formatter-001" name="1" type="text" class="form-control" placeholder data-ax5formatter="phone" value="01012345678">\n' +
+			'    </div>\n' +
+			'</div>\n' +
+			'<div class="form-group">\n' +
+			'    <div class="input-group">\n' +
+			'        <span class="input-group-addon">Money</span>\n' +
+			'        <input id="ax5formatter-002" name="2" type="text" class="form-control" placeholder="02-XXXX-XXXX" data-ax5formatter="phone" value="0212345678">\n' +
+			'    </div>\n' +
+			'</div>');
+	});
+
+	it('Formatter Phone' , function(){
+		$('[data-ax5formatter]').ax5formatter();
+
+		$('#ax5formatter-001').val().should.equal('010-1234-5678');
+		$('#ax5formatter-002').val('0298765432').blur().val().should.equal('02-9876-5432');
+	});
+
+	after(function(){
+		$('div.form-group').remove();
+	});
+});
+
+describe('formatter Bizno TEST', function() {
+	before(function(){
+		$('body').append(
+			'<div class="form-group" id="ax5formatter-01">\n' +
+			'    <div class="input-group">\n' +
+			'        <span class="input-group-addon">Money</span>\n' +
+			'        <input id="ax5formatter-001" name="1" type="text" class="form-control" placeholder data-ax5formatter="bizno" value="1234567890">\n' +
+			'    </div>\n' +
+			'</div>\n' +
+			'<div class="form-group">\n' +
+			'    <div class="input-group">\n' +
+			'        <span class="input-group-addon">Money</span>\n' +
+			'        <input id="ax5formatter-002" name="2" type="text" class="form-control" placeholder="XXX-XX-XXXXX" data-ax5formatter="bizno" value="1234567890">\n' +
+			'    </div>\n' +
+			'</div>');
+	});
+
+	it('Formatter Bizno' , function(){
+		$('[data-ax5formatter]').ax5formatter();
+
+		$('#ax5formatter-001').val().should.equal('123-45-67890');
+		$('#ax5formatter-002').val('0987654321').blur().val().should.equal('098-76-54321');
+	});
+
+	after(function(){
+		$('div.form-group').remove();
+	});
+});
