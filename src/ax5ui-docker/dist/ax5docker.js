15781ccbe6f7462e3e70102daaeab0808ef83d18
Thomas Jang <tom@axisj.com>
2017-01-22 22:45:45 +0900

    docker 1/5

--- src/ax5ui-docker/dist/ax5docker.js
+++ src/ax5ui-docker/dist/ax5docker.js
@@ -13,7 +13,7 @@
 
     UI.addClass({
         className: "docker",
-        version: "1.3.81"
+        version: "${VERSION}"
     }, function () {
 
         /**
@@ -26,24 +26,60 @@
          * ```
          */
         var ax5docker = function ax5docker() {
+            var _this = this;
 
             var self = this,
                 cfg = void 0;
 
             this.instanceId = ax5.getGuid();
-            this.config = {};
+            this.config = {
+                theme: 'default',
+                animateTime: 250,
+                columnKeys: {}
+            };
             // 패널 정보
             this.panels = [];
             cfg = this.config;
 
+            var repaintPanels = function repaintPanels() {
+                console.log(_this.config.target);
+                _this.$target.html(DOCKER.tmpl.get("panels", { panels: _this.panels }));
+            };
+
+            /**
+             * @method ax5docker.setConfig
+             * @param {Object} config
+             * @param {Array} config.panels
+             */
+            this.init = function (_config) {
+                cfg = jQuery.extend(true, {}, cfg, _config);
+                if (!cfg.target) {
+                    console.log(ax5.info.getError("ax5docker", "401", "init"));
+                    return this;
+                }
+                // memory target
+                this.$target = jQuery(cfg.target);
+                // set panels
+                this.panels = cfg.panels || [];
+                // event Functions
+                this.onStateChanged = cfg.onStateChanged;
+                this.onClick = cfg.onClick;
+                this.onLoad = cfg.onLoad;
+                this.onDataChanged = cfg.body.onDataChanged;
+
+                // 패널 다시 그리기
+                repaintPanels();
+            };
             /**
              * @method ax5docker.setPanels
              * @returns {ax5docker}
              */
             this.setPanels = function (_panels) {
+                // set panels
+                this.panels = _panels || [];
 
-                console.log(_panels);
-
+                // 패널 다시 그리기
+                repaintPanels();
                 return this;
             };
 
@@ -54,6 +90,23 @@
                 }
             }.apply(this, arguments);
         };
+
         return ax5docker;
     }());
+})();
+// ax5.ui.docker.tmpl
+(function () {
+
+    var DOCKER = ax5.ui.docker;
+
+    var panels = function panels(columnKeys) {
+        return " \n        \n        ";
+    };
+
+    DOCKER.tmpl = {
+        "panels": panels,
+        get: function get(tmplName, data, columnKeys) {
+            return ax5.mustache.render(DOCKER.tmpl[tmplName].call(this, columnKeys), data);
+        }
+    };
 })();
--- src/ax5ui-docker/src/ax5docker.js
+++ src/ax5ui-docker/src/ax5docker.js
@@ -24,29 +24,61 @@
          * ```
          */
         let ax5docker = function () {
-
             let self = this,
                 cfg;
 
             this.instanceId = ax5.getGuid();
             this.config = {
-
+                theme: 'default',
+                animateTime: 250,
+                columnKeys: {}
             };
             // 패널 정보
             this.panels = [];
             cfg = this.config;
 
+            let repaintPanels = () => {
+                console.log(this.config.target);
+                this.$target.html(DOCKER.tmpl.get("panels", {panels: this.panels}));
+            };
+
+
+            /**
+             * @method ax5docker.setConfig
+             * @param {Object} config
+             * @param {Array} config.panels
+             */
+            this.init = function (_config) {
+                cfg = jQuery.extend(true, {}, cfg, _config);
+                if (!cfg.target) {
+                    console.log(ax5.info.getError("ax5docker", "401", "init"));
+                    return this;
+                }
+                // memory target
+                this.$target = jQuery(cfg.target);
+                // set panels
+                this.panels = cfg.panels || [];
+                // event Functions
+                this.onStateChanged = cfg.onStateChanged;
+                this.onClick = cfg.onClick;
+                this.onLoad = cfg.onLoad;
+                this.onDataChanged = cfg.body.onDataChanged;
+
+                // 패널 다시 그리기
+                repaintPanels();
+            };
             /**
              * @method ax5docker.setPanels
              * @returns {ax5docker}
              */
             this.setPanels = function (_panels) {
-                
-                console.log(_panels);
-                
+                // set panels
+                this.panels = _panels || [];
+
+                // 패널 다시 그리기
+                repaintPanels();
                 return this;
             };
-
 
             // 클래스 생성자
             this.main = (function () {
@@ -56,6 +88,7 @@
             }).apply(this, arguments);
 
         };
+
         return ax5docker;
     })());
 
