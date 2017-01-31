8c4fa8fe4f6a86f63cba110e24bdebc8d5e471e6
aeei <khm@imtsoft.org>
2017-01-31 18:45:22 +0900

    update test code

--- src/ax5ui-layout/test/test.layout.js
+++ src/ax5ui-layout/test/test.layout.js
@@ -5,7 +5,6 @@
  */
 
 describe('ax5layout TEST', function () {
-
     var tmpl = '<div data-ax5layout="ax1" data-config="{layout:\'dock-panel\'"}" style="height: 500px;border:1px solid #ccc;">' +
         '<div data-dock-panel="{dock:\'top\', split:true, height: 100, maxHeight: 300}" style="text-align: center;"><h4>TOP</h4></div>' +
         '<div data-dock-panel="{dock:\'bottom\', split:"true", height: 100, minHeight: 10, maxHeight: 300}" style="text-align: center;"><h4>BOTTOM</h4></div>' +
@@ -17,11 +16,60 @@ describe('ax5layout TEST', function () {
         '</div>';
 
     $(document.body).append(tmpl);
-
     ///
     it('bind ax5layout', function (done) {
-        jQuery('[data-ax5layout]').ax5layout();
+        jQuery('[data-ax5layout="ax1"]').ax5layout();
         done(ax5.ui.layout_instance.queue.length == 1 ? "" : "bind layout error");
     });
 
+    after(function () {
+        jQuery('[data-ax5layout="ax1"]').remove();
+    });
+});
+
+describe('ax5layout method TEST', function () {
+    var myUI;
+    var tmpl;
+    var that;
+
+    before(function () {
+        myUI = new ax5.ui.layout();
+        tmpl = '<div data-ax5layout="ax1" data-config="{layout:\'dock-panel\'"}" style="height: 500px;border:1px solid #ccc;">' +
+            '<div data-dock-panel="{dock:\'top\', split:true, height: 100, maxHeight: 300}" style="text-align: center;"><h4>TOP</h4></div>' +
+            '<div data-dock-panel="{dock:\'bottom\', split:"true", height: 100, minHeight: 10, maxHeight: 300}" style="text-align: center;"><h4>BOTTOM</h4></div>' +
+            '<div data-dock-panel="{dock:\'left\', split:true, width: 100, minWidth: 10, maxWidth: 300}"><h4>LEFT</h4></div>' +
+            '<div data-dock-panel="{dock:\'right\', split:true, width: 100, minWidth: 10, maxWidth: 300}"><h4>RIGHT</h4></div>' +
+            '<div data-dock-panel="{dock:\'center\'}" style="padding: 5px;text-align: center;">' +
+            '<h4>CENTER</h4>' +
+            '</div>' +
+            '</div>';
+
+        $(document.body).append(tmpl);
+    });
+
+    it('bind ax5layout', function (done) {
+        myUI.bind({
+            target: $('[data-ax5layout="ax1"]'),
+            layout: "dock-panel",
+            onResize: function () {
+                that = this;
+            }
+        });
+        done(ax5.ui.layout_instance.queue.length == 1 ? "" : "bind layout error");
+    });
+
+    it('resize ax5layout', function (done) {
+        myUI.resize($('[data-ax5layout="ax1"]'), {top: 20, bottom: 30, left: 50, right: 70});
+        setTimeout(function () {
+            done(
+                ae.equalAll(20, that.dockPanel.top.__height)
+                || ae.equalAll(50, that.dockPanel.left.__width)
+                || ae.equalAll(70, that.dockPanel.right.__width)
+            );
+        }, myUI.config.animateTime);
+    });
+
+    after(function () {
+       jQuery('[data-ax5layout="ax1"]').remove();
+    });
 });
