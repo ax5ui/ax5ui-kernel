20c6b2bc19e4d65b49b4a11a5a5debcbdabd7071
aeei <khm@imtsoft.org>
2017-04-14 22:23:56 +0900

    update test code

--- src/ax5core/test/test.core.date.js
+++ src/ax5core/test/test.core.date.js
@@ -59,6 +59,13 @@ describe('ax5.util.date TEST', function () {
         should.deepEqual(ax5.util.date("201704"), date);
     });
 
+    //Usage 09
+    it('ax5.util.date("2017") [string.length > 2]', function () {
+        var date = new Date(2017, 0);
+        date.setHours(12);
+        should.deepEqual(ax5.util.date("2017"), date);
+    });
+
     /* end ax5.util.date */
 });
 
