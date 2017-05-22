ba06d45050ce0ccfbb34c870653e7160e653ec5a
aeei <aeei@axisj.com>
2017-05-22 22:29:18 +0900

    update test code

--- src/ax5core/test/test.core.number.js
+++ src/ax5core/test/test.core.number.js
@@ -1,89 +1,99 @@
-describe('ax5.util.number TEST', function() {
+describe('ax5.util.number TEST', function () {
     var testCases = [
         {
             args: [
-              123456789.678,
-              {
-                round: 1
-              }
+                123456789.678,
+                {
+                    round: 1
+                }
             ],
             expect: 123456789.7,
             explanation: 123456789.678 + ', { round: 1 }'
         },
         {
             args: [
-              123456789.678,
-              {
-                round: 1,
-                money: true
-              }
+                123456789.678,
+                {
+                    round: 1,
+                    money: true
+                }
             ],
             expect: '123,456,789.7',
             explanation: 123456789.678 + ', { round: 1, money: true }'
         },
         {
             args: [
-              123456789.678,
-              {
-                round: 2,
-                byte: true
-              }
+                123456789.678,
+                {
+                    round: 2,
+                    byte: true
+                }
             ],
             expect: '117.7MB',
             explanation: 123456789.678 + ', { round: 2, byte: true }'
         },
         {
             args: [
-              -123456789.678,
-              {
-                abs: true,
-                round: 2,
-                money: true
-              }
+                -123456789.678,
+                {
+                    abs: true,
+                    round: 2,
+                    money: true
+                }
             ],
             expect: '123,456,789.68',
             explanation: -123456789.678 + ',{ abs: true, round: 2, money: true }'
         },
         {
             args: [
-              -123456789.678,
-              {
-                abs: true,
-                ceil: true,
-                money: true
-              }
+                -123456789.678,
+                {
+                    abs: true,
+                    ceil: true,
+                    money: true
+                }
             ],
             expect: '123,456,790',
             explanation: -123456789.678 + ',{ abs: true, ceil: true, money: true }'
         },
         {
             args: [
-              -123456789.678,
-              {
-                abs: true,
-                floor: true,
-                money: true
-              }
+                -123456789.678,
+                {
+                    abs: true,
+                    floor: true,
+                    money: true
+                }
             ],
             expect: '123,456,789',
             explanation: -123456789.678 + ',{ abs: true, floor: true, money: true }'
         },
         {
             args: [
-              'A-1234~~56789.8~888PX',
-              {
-                abs: true,
-                round: 2,
-                money: true
-              }
+                1024 * 1024,
+                {
+                    byte: true
+                }
+            ],
+            expect: '1024KB',
+            explanation: 1024 * 1024 + ',{ byte: true, floor: true}'
+        },
+        {
+            args: [
+                'A-1234~~56789.8~888PX',
+                {
+                    abs: true,
+                    round: 2,
+                    money: true
+                }
             ],
             expect: '123,456,789.89',
             explanation: 'A-1234~~56789.8~888PX , { abs: true, round: 2, money: true }'
         }
     ];
 
-    testCases.forEach(function(testCase){
-        it('ax5.util.number(' + testCase.explanation + ') expect ' + testCase.expect, function() {
+    testCases.forEach(function (testCase) {
+        it('ax5.util.number(' + testCase.explanation + ') expect ' + testCase.expect, function () {
             var actual = ax5.util.number.apply(this, testCase.args);
 
             actual.should.deepEqual(testCase.expect);
