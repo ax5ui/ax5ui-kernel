d809121b19db85c121a13bbbbd771507d8605618
Thomas Jang <tom@axisj.com>
2017-07-31 16:07:40 +0900

    1.4.101 RELEASED

--- src/ax5core/dist/ax5core.js
+++ src/ax5core/dist/ax5core.js
@@ -55,7 +55,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
          * ax5 version
          * @member {String} ax5.info.version
          */
-        var version = "1.4.100";
+        var version = "1.4.101";
 
         /**
          * ax5 library path
