94a337d6679e2e8f90e7cd61d8429b7f184d9fb1
Thomas Jang <tom@axisj.com>
2019-04-10 12:49:31 +0900

    picker disabled기능 추가.

--- src/ax5core/dist/ax5core.js
+++ src/ax5core/dist/ax5core.js
@@ -55,7 +55,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
          * ax5 version
          * @member {String} ax5.info.version
          */
-        var version = "1.4.130";
+        var version = "1.4.131";
 
         /**
          * ax5 library path
